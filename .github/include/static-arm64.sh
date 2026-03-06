6a889c0880bd48e7e670457debef4121ad8ebf36
CalvinXu17 <50015746+CalvinXu17@users.noreply.github.com>
2026-03-06 13:39:13 +0800

    Update static-arm64.sh

--- .github/include/static-arm64.sh
+++ .github/include/static-arm64.sh
@@ -17,18 +17,10 @@ docker build -t "$IMAGE" -f docker/Dockerfile.static docker/
 docker run -v $(pwd):$(pwd) -w $(pwd) -i "$IMAGE" <<'EOF'
 set -eux
 BUILD_DIR=build-static
-cmake -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE=Release -DCMAKE_VERBOSE_MAKEFILE=ON -DBUILD_TESTING=OFF -DSTATIC_LINKING=ON -DENABLE_SKB_OUTPUT=OFF
+cmake -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE=Release -DCMAKE_VERBOSE_MAKEFILE=ON -DBUILD_TESTING=OFF -DCMAKE_EXE_LINKER_FLAGS="-static" -DSTATIC_LINKING=ON -DENABLE_SKB_OUTPUT=OFF
 make -C "$BUILD_DIR" -j$(nproc)
 
 # Basic smoke test
 ./"$BUILD_DIR"/src/bpftrace --help
 
-# Validate that it's a mostly static binary except for libc
-EXPECTED="/lib/ld-musl-aarch64.so.1\nlibc.musl-aarch64.so.1"
-GOT=$(ldd "$BUILD_DIR"/src/bpftrace | awk '{print $1}')
-if ! diff <(echo -e "$EXPECTED") <(echo "$GOT"); then
-  set +x
-  >&2 echo "bpftrace incorrectly linked"
-  exit 1
-fi
 EOF
